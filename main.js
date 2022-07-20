94607754a857b23f89185971f0ab1543d2b25e98
Jiwoo <wldn0804@gmail.com>
2022-07-20 16:45:57 +0900

    게임 완성

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 // 변수
 const DEFALUT_TIME = 3;
-let score = 0;
-let time = DEFALUT_TIME;
+let words = [];
+let time;
 let isPlaying = false;
+let score = 0;
 let timeInterval;
 let checkInterval;
-let words = [];
 
-let button = document.querySelector(".button");
 let timeDisplay = document.querySelector(".time");
-let scoreDisplay = document.querySelector(".score");
-let wordInput = document.querySelector(".word-input")
+let button = document.querySelector(".button");
 let wordDisplay = document.querySelector('.word-display');
+let wordInput = document.querySelector(".word-input")
+let scoreDisplay = document.querySelector(".score");
 
 // 초기 세팅 함수
 init()
-
 function init() {
+    buttonChange('loading...')
     getWords();
     wordInput.addEventListener('input', checkMatch);
     button.addEventListener('click', buttonChange);
@@ -24,6 +24,7 @@ function init() {
 
 // 게임 실행
 function run() {
+    if(isPlaying) return;
     isPlaying = true;
     time = DEFALUT_TIME;
     wordInput.focus();
@@ -32,38 +33,43 @@ function run() {
     timeInterval = setInterval(countDown, 1000);
     checkInterval = setInterval(checkStatus, 50);
     buttonChange('게임중');
-    console.log(button.innerText)
 }
 
 function checkStatus() {
     if(!isPlaying && time === 0) {
-        buttonChange('게임 종료');
+        buttonChange('게임 시작');
         clearInterval(checkInterval);
     } 
 }
 
+// 단어 랜덤으로 불러오는 함수
+function getWords() {
+    axios.get('https://random-word-api.herokuapp.com/word?number=100')
+    .then(function (response) {
+        response.data.forEach((word) => {
+            if(word.length < 10) words.push(word); 
+        })
+        buttonChange('게임 시작');
+    })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
 // 단어 일치 체크 함수
 function checkMatch() {
+    // if(!isPlaying) return;
     if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()) {
         wordInput.value = ''; // 단어 초기화
         if(!isPlaying) return;
         score ++; //  단어와 일치하면 획득점수 +1
         scoreDisplay.innerText = score;
-        time = DEFALUT_TIME;
+        // time = DEFALUT_TIME;
         //  단어 랜덤으로 불러옴
         const randomIndex = Math.floor(Math.random() * words.length);
         wordDisplay.innerText = words[randomIndex];
     }
 }
-
-
-// 단어 랜덤으로 불러오는 함수
-function getWords() {
-    words = ['apple', 'orange', 'grape', 'banana', 'lemon', 'plum'];
-    buttonChange('게임 시작');
-}
-
-// 버튼 클릭 시 실행 함수
 
 // 시간 흐르는 함수
 function countDown() {
@@ -77,5 +83,3 @@ function buttonChange(text) {
     button.innerText = text;
     text === "게임 시작" ? button.classList.remove('loading') : button.classList.add("loading");
 }
-
-// 게임중 으로 안나오는거 해결중!!
